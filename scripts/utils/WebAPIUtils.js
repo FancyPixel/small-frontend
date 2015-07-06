b7001134103598c85c058417759b7884b3609e5a
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-07-06 17:04:51 +0200

    Fix authentication

--- scripts/utils/WebAPIUtils.js
+++ scripts/utils/WebAPIUtils.js
@@ -42,7 +42,7 @@ module.exports = {
 
   login: function(email, password) {
     request.post(APIEndpoints.LOGIN)
-      .send({ username: email, password: password, grant_type: 'password' })
+      .send({ email: email, password: password, grant_type: 'password' })
       .set('Accept', 'application/json')
       .end(function(error, res){
         if (res) {
